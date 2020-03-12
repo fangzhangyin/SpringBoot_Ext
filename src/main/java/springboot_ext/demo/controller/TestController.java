aede06949eae45113447b220cc070491db7c77ca
fzy <2570413931@qq.com>
2020-03-12 21:56:53 +0800

    thymeleaf简单的使用

--- src/main/java/springboot_ext/demo/controller/TestController.java
+++ src/main/java/springboot_ext/demo/controller/TestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import springboot_ext.demo.entity.user;
 
+import java.util.Map;
+
 @Controller
 public class TestController {
 
@@ -22,5 +24,12 @@ public class TestController {
     @ResponseBody
     public String user(){
         return user.getUsername();
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){//应用map将自动返回到http的相应前台（可用来传值）
+        //若引用thymeleaf，则自动去templates文件夹下去寻找success.html
+        map.put("hello","我是后台响应回来的值");
+        return "success";
     }
 }
